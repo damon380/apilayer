2deb482d21e25f7a67446e11846932897caa48df
damon380 <damonchong380@gmail.com>
2022-03-08 02:17:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,7 @@
 import streamlit as st
 menu_items = {
 	'Get help': 'http://techmeme.com',
-	'Report a bug': 'http://woolwich.xyz',
-	'About': '''
-	 ## My Custom App
-
-	 Some markdown to show in the About dialog.
-	'''
-}
+	'Report a bug?': 'http://woolwich.xyz',
 
 st.set_page_config(menu_items=menu_items)
 st.write("Apilayer")
