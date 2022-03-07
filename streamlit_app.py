74afc5bead5af8ed9122e89557f18bf904d90c05
damon380 <damonchong380@gmail.com>
2022-03-08 02:15:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,13 @@
 import streamlit as st
+menu_items = {
+	'Get help': YOUR_HELP_URL_STRING,
+	'Report a bug': YOUR_BUG_PAGE_URL_STRING,
+	'About': '''
+	 ## My Custom App
+
+	 Some markdown to show in the About dialog.
+	'''
+}
+
+st.set_page_config(menu_items=menu_items)
 st.write("Apilayer")
