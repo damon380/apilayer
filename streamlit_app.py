34a06d254eaebd10a740d1832a3fa9318953f0ce
damon380 <damonchong380@gmail.com>
2022-03-08 02:18:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 menu_items = {
 	'Get help': 'http://techmeme.com',
-	'Report a bug?': 'http://woolwich.xyz',
+	'Report a bug?': 'http://woolwich.xyz'
+}
 
 st.set_page_config(menu_items=menu_items)
 st.write("Apilayer")
