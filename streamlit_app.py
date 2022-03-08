4cc51575ab488094e726972d88162e15f5f604dd
damon380 <damonchong380@gmail.com>
2022-03-09 01:46:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 c1, c2 = st.columns((1, 1))
 
 c1.text_input('Enter your website url', 'http://www.gmail.com',48)
